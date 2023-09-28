45978eb37a67da7379d3cfe8af0d890c3b378ee3
alextepper <alextepper92@gmail.com>
2023-09-28 12:02:49 +0300

    another try

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ const postRoute = require("./routes/posts");
 
 donenv.config();
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB", err);
   }
-);
+};
+
+connectDB();
 
 app.use(express.json());
 app.use(helmet());
