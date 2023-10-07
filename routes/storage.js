fbe7c4f92f71ee0410b50617373d7a37c73bee35
alextepper <alextepper92@gmail.com>
2023-10-07 12:52:28 +0300

    saving to the DB

--- routes/storage.js
+++ routes/storage.js
@@ -14,12 +14,31 @@ cloudinary.config({
 const storage = multer.memoryStorage(); // Use memory storage
 const upload = multer({ storage: storage });
 
+async function saveImageUrlToDb(username, imageUrl) {
+  // Find the user by username
+  const user = await User.findOne({ username: username });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  // Update the user's image URL
+  user.profilePicture = imageUrl;
+
+  // Save the user
+  await user.save();
+
+  return user;
+}
+
 router.post("/upload", upload.single("userImage"), async (req, res) => {
   try {
     // Upload to Cloudinary
     const result = await cloudinary.uploader
       .upload_stream(
         {
+          resource_type: "auto",
+          folder: `fut-card-profile-pics/${req.body.username}`,
           transformation: [
             { height: 350, crop: "fit" },
             { gravity: "auto:face", height: 350, width: 250, crop: "crop" },
@@ -31,6 +50,10 @@ router.post("/upload", upload.single("userImage"), async (req, res) => {
         }
       )
       .end(req.file.buffer);
+
+    const imageUrl = result.secure_url;
+
+    const updatedUser = await saveImageUrlToDb(req.body.username, imageUrl);
   } catch (error) {
     res.status(500).json({ error: "Failed to upload image" });
   }
